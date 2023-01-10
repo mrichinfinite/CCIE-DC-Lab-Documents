ee6d004624c36858046e83a2fb3af9e4c93e0348
Matthew Rich <matthew.rich@mrichinfinite.com>
2023-01-10 10:00:59 -0600

    Add show_vlans.py

---
+++ NXOS_Python_Scripts/show_vlans.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+import requests
+import json
+
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
+
+# Edit these values
+url = 'https://<ip-address-or-hostname>/ins'
+username = ''
+password = ''
+
+headers = {'content-type': 'application/json'}
+payload = {
+    'ins_api': {
+        'version': '1.0',
+        'type': 'cli_show',
+        'chunk': '0',
+        'sid': '1',
+        'input': 'show vlan brief',
+        'output_format': 'json',
+    }
+}
+response = requests.post(
+    url,
+    data=json.dumps(payload),
+    headers=headers,
+    auth=(username, password),
+    verify=False,
+).json()
+
+vlan_list = response['ins_api']['outputs']['output']['body'][
+    'TABLE_vlanbriefxbrief'
+]['ROW_vlanbriefxbrief']
+
+for vlan in vlan_list:
+    print('{}'.format(vlan['vlanshowbr-vlanname']))
