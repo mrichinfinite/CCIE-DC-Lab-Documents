b09259a08cc10c1d10815ee046099ade3a4968f1
Matthew Rich <matthew.rich@mrichinfinite.com>
2023-01-12 09:38:55 -0600

    Add ACI Python scripts

---
+++ ACI_Python_Scripts/create_tenant.py
@@ -0,0 +1,32 @@
+# Imports
+import cobra.mit.access
+import cobra.mit.request
+import cobra.mit.session
+import cobra.model.fv
+import cobra.model.pol
+import cobra.model.vns
+from cobra.internal.codec.xmlcodec import toXMLStr
+import sys
+
+# Count args
+args = len(sys.argv)
+
+# Log into APIC and create dir object
+ls = cobra.mit.session.LoginSession('https://<apic-ip-address-or-hostname>', '<username>', '<password>')
+md = cobra.mit.access.MoDirectory(ls)
+md.login()
+
+# Top level object on which operations will be made
+polUni = cobra.model.pol.Uni('')
+
+# Build the request and take new Tenant name as argument
+for i in range(1, args):
+    fvTenant = cobra.model.fv.Tenant(polUni, ownerkey='', name=sys.argv[i], descr='', userdom=':all:', nameAlias='', OwnerTag='', annotation='')
+    vnsSvcCont = cobra.model.vns.SvcCont(fvTenant, userdom=':all:', annotation='')
+    fvRsTenantMonPol = cobra.model.fv.RsTenantMonPol(fvTenant, userdom=':all:', annotation='', tnMonEPGPolName='')
+
+# Commit generated code to APIC
+print(toXMLStr(polUni))
+c = cobra.mit.request.ConfigRequest()
+c.addMo(polUni)
+md.commit(c)
